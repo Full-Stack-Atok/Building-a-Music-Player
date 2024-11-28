44f83ebd865e7dd270ae7267cf4aeb59a09acf91
Full-Stack-Atok <komsaykerby@gmail.com>
2024-11-28 12:41:02 +0800

    Day 6

--- script.js
+++ script.js
@@ -112,7 +112,33 @@ const playPreviousSong = () => {
 
 const highlightCurrentSong = () => {
     const playlistSongElements = document.querySelectorAll(".playlist-song");
+    const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+    playlistSongElements.forEach((songEl) => {
+        songEl.removeAttribute("aria-current");
+    });
+    
+    if(songToHighlight) {
+        songToHighlight.setAttribute("aria-current", "true");
+    }
 }
+
+const setPlayerDisplay = () => {
+    const playingSong = document.getElementById("player-song-title");
+    const songArtist = document.getElementById("player-song-artist");
+    const currentTitle = userData?.currentSong?.title;
+    const currentArtist = userData?.currentSong?.artist;
+
+   playingSong.textContent = currentTitle ? currentTitle : "";
+   songArtist.textContent = currentArtist ? currentArtist : "";
+
+}
+
+const setPlayButtonAccessibleText = () => {
+    const song = userData?.currentSong || userData?.songs[0];
+    playButton.setAttribute("aria-label", song?.title ? `Play${song.title}` : "Play");
+}
+
+
 // Play Function
 const playSong = (id) => {
     const song = userData?.songs.find((song) => song.id === id);
@@ -126,6 +152,9 @@ const playSong = (id) => {
     };
     userData.currentSong = song;
     playButton.classList.add("playing");
+
+    highlightCurrentSong();
+    setPlayerDisplay();
     audio.play();
 }
 
